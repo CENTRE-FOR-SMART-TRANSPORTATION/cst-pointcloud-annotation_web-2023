2ee170b8d188293aeebf3a13daa453d6ca31b18b
Gurveer Sohal <gurveersohal433@gmail.com>
2023-10-13 14:47:02 -0600

    fine

--- public/js/editor.js
+++ public/js/editor.js
@@ -118,7 +118,7 @@ function Editor(editorUi, wrapperUi, editorCfg, data, name = "editor") {
       console.log(data.centre)
 
       if (data.centre == null) {
-        alert('Docker setup was succesul')
+        alert('No location found for this point cloud')
         return
       }
 
